91b37a522f989dfa579a541c4a7287c94e27a607
Eduardo de Lucca <eduardo.luccati@gmail.com>
2026-03-29 17:53:12 -0300

    Class Estado - Ajuste de compilação

--- gestao_scrum/Estado.cpp
+++ gestao_scrum/Estado.cpp
@@ -1,5 +1,11 @@
 #include "Estado.hpp"
 
+using namespace std;
+
+const string Estado::A_FAZER = "A FAZER";
+const string Estado::FAZENDO = "FAZENDO";
+const string Estado::FEITO = "FEITO";
+
 void Estado::setEstado(string estado){
     if(estado != A_FAZER || estado != FAZENDO || estado != FEITO){
 
--- gestao_scrum/Estado.hpp
+++ gestao_scrum/Estado.hpp
@@ -1,14 +1,15 @@
 #ifndef ESTADO_HPP_INCLUDED
 #define ESTADO_HPP_INCLUDED
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Estado {
     private:
-        static const string A_FAZER = "A FAZER";
-        static const string FAZENDO = "FAZENDO";
-        static const string FEITO = "FEITO";
+        static const string A_FAZER;
+        static const string FAZENDO;
+        static const string FEITO;
 
         string estado;
     public:
